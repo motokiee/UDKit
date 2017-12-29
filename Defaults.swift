c7bee1d009ed55330d246155cc5592114f61bc3d
motokiee <m.nat44@gmail.com>
2017-12-29 18:18:55 +0900

    Add Defaults

---
+++ Defaults.swift
@@ -0,0 +1,28 @@
+//
+//  UserDefaults.swift
+//  UDKit
+//
+//  Created by motokiee on 2017/12/29.
+//
+
+import Foundation
+
+public struct Key<Value: Codable> {
+    internal let key: String
+
+    public init(_ key: String) {
+        self.key = key
+    }
+}
+
+
+public final class Defaults {
+    private static let userDefaults = UserDefaults.standard
+    private init() {}
+
+    public static func clear(key: Key<Value>) -> Value {
+        userDefaults.set(nil, forKey: key.key)
+        userDefaults.synchronize()
+    }
+
+}
---
+++ UDKit/UDKit/UDKit.h
@@ -0,0 +1,19 @@
+//
+//  UDKit.h
+//  UDKit
+//
+//  Created by motokiee on 2017/12/29.
+//  Copyright © 2017年 motokiee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UDKit.
+FOUNDATION_EXPORT double UDKitVersionNumber;
+
+//! Project version string for UDKit.
+FOUNDATION_EXPORT const unsigned char UDKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UDKit/PublicHeader.h>
+
+
